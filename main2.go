a9a450ff8f3f4937f4c1b1018201ef826f40b717
berhanu172 <beri44577@gmail.com>
2026-03-26 16:34:44 +0300

    Add files via upload
    ok

--- main2.go
+++ main2.go
@@ -2,8 +2,21 @@ package main
 
 import (
 	"fmt"
+	//"log"
+
+	//"os"
+	"net/http"
 )
 
 func main() {
-	fmt.Println("hello world")
+	//fmt.Println("hello world")
+
+	//portstring := os.Getenv("port")
+	//if portstring == "" {
+	//log.Fatal("port is not found")
+	//}
+	//fmt.Println("port:", portstring)
+
+	fmt.Println("listining  on port 8000")
+	http.ListenAndServe(":8000", nil)
 }
