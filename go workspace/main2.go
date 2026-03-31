5ebf858c7fc9e0bd2af7d8af61da2d2f07dff654
berhanu172 <beri44577@gmail.com>
2026-03-31 16:45:49 +0300

    Add files via upload
    ok

--- go workspace/main2.go
+++ go workspace/main2.go
@@ -3,19 +3,34 @@ package main
 import (
 	"fmt"
 	"log"
-
+	"net/http"
 	"os"
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/cors"
+	"github.com/joho/godotenv"
 )
 
 func main() {
+
 	fmt.Println("hello world")
 
+	godotenv.Load()
+	
+	
+
 	portstring := os.Getenv("port")
-	if portstring == "" {
+	if portstring == "" { 
 		log.Fatal("port is not found")
 	}
+		r:=chi.NewRouter()
+ serv:= &http.server{
+		Handler:r,
+		Addr:   ":" +   portstring,
+	}
+	log.Printf("server starting on port"%v,portstring)
+	err := serv.ListenAndServe()
+	if err!=nil{
+		log.Fatal(err)
 	fmt.Println("port:", portstring)
 
-	//fmt.Println("listining  on port 8000")
-	//http.ListenAndServe(":8000", nil)
-}
+	}
